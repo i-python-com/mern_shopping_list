78173a74b94761a59b2b60b2fb2303ac16da770d
patrick <wyp4wj@gmail.com>
2019-09-08 16:00:57 -0700

    Implement responsesive navbar by reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
+import AppNavbar from './components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavbar />
+      </div>
+    )
+  }
 }
 
 export default App
