5b1fd545e5fab70c80171107a794a0a2692fea29
patrick <wyp4wj@gmail.com>
2019-09-09 16:44:32 -0700

    Implement auth middleware and auth user route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const bcrypt = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
+const auth = require('../../middleware/auth')
 
 // user model
 const User = require('../../models/User')
@@ -46,6 +47,15 @@ router.post('/', (req, res) => {
       )
     })
   })
+})
+
+// @route GET api/auth/user
+// @desc Get user data
+// @access Private
+router.get('/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user))
 })
 
 module.exports = router
