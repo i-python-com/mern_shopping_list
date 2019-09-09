c6411d1030245c1340911bd661485c95a865ede6
patrick <wyp4wj@gmail.com>
2019-09-09 13:35:22 -0700

    prepared to deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
@@ -20,6 +21,16 @@ mongoose
 
 // use routes
 app.use('/api/items', items)
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build'))
+  })
+}
 
 const port = process.env.PORT || 5000
 
