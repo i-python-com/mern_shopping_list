ecaafb584487e376309236ca0b82baa812441862
patrick <wyp4wj@gmail.com>
2019-09-09 22:29:15 -0700

    Implement LoginModal and login action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -63,6 +63,36 @@ export const register = ({ name, email, password }) => dispatch => {
     })
 }
 
+// login user
+export const login = ({ email, password }) => dispatch => {
+  // headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  // request body
+  const body = JSON.stringify({ email, password })
+
+  axios
+    .post('/api/auth', body, config)
+    .then(res =>
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL')
+      )
+      dispatch({
+        type: LOGIN_FAIL
+      })
+    })
+}
+
 // logout user
 export const logout = () => {
   return {
