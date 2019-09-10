9b06a1464e4b50580dab721103eb7b90b3837403
patrick <wyp4wj@gmail.com>
2019-09-09 23:15:28 -0700

    Implement isAuthenticated in ShoppingList and ItemModel

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -11,11 +11,16 @@ import {
 } from 'reactstrap'
 import { connect } from 'react-redux'
 import { addItem } from '../actions/itemActions'
+import PropTypes from 'prop-types'
 
 class ItemModal extends Component {
   state = {
     modal: false,
     name: ''
+  }
+
+  static propTypes = {
+    isAuthenticated: PropTypes.bool
   }
 
   toggle = () => {
@@ -45,14 +50,17 @@ class ItemModal extends Component {
   render() {
     return (
       <div>
-        <Button
-          color="dark"
-          style={{ marginBottom: '2rem' }}
-          onClick={this.toggle}
-        >
-          Add Item
-        </Button>
-
+        {this.props.isAuthenticated ? (
+          <Button
+            color="dark"
+            style={{ marginBottom: '2rem' }}
+            onClick={this.toggle}
+          >
+            Add Item
+          </Button>
+        ) : (
+          <h4 className="mb-3 ml-4">Please login to manage items</h4>
+        )}
         <Modal isOpen={this.state.modal} toggle={this.taggle}>
           <ModalHeader toggle={this.toggle}>Add To Shopping List</ModalHeader>
           <ModalBody>
@@ -79,7 +87,8 @@ class ItemModal extends Component {
 }
 
 const mapStateToProps = state => ({
-  item: state.item
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(
