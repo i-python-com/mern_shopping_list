bb3e8679d22e97b848ec5c978e0902802d97854e
patrick <wyp4wj@gmail.com>
2019-09-09 18:57:00 -0700

    Implement register modal and error handle

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -9,6 +9,7 @@ import {
   NavLink,
   Container
 } from 'reactstrap'
+import RegisterModal from './auth/RegisterModal'
 
 class AppNavbar extends Component {
   state = {
@@ -31,9 +32,7 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/bradtraversy">
-                    Github
-                  </NavLink>
+                  <RegisterModal />
                 </NavItem>
               </Nav>
             </Collapse>
